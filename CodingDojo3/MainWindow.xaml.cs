0924c6c5356701b1be01ac31c88b8902c02dc9f5
Sascha Böck <wi16b142@technikum-wien.at>
2017-10-11 01:18:07 +0200

    project created layout of main window created

--- CodingDojo3/MainWindow.xaml.cs
+++ CodingDojo3/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace CodingDojo3
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
