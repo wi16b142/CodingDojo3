36be9f86d9cd090098c882502a290f18fb544269
Sascha Böck <wi16b142@technikum-wien.at>
2017-10-22 10:38:01 +0200

    Changed folder structure

--- CodingDojo3/Converters/StockToBrushConverter.cs
+++ CodingDojo3/Converters/StockToBrushConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
 
-namespace CodingDojo3
+namespace CodingDojo3.Converters
 {
     public class StockToBrushConverter : IValueConverter
     {
